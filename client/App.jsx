2645c02599b447de300be0e8fed84bf7ebb400cf
taadams1 <rainedoescode@gmail.com>
2022-09-01 23:19:53 -0500

    Animation tests

--- client/App.jsx
+++ client/App.jsx
@@ -6,10 +6,14 @@ const App = () => {
   return (
     <div class="flex flex-col w-full h-full">
       <Navigation />
-      <main class="flex max-w-full max-h-full"><h1>App Test</h1></main>
+      <main class="flex flex-col flex-grow">
+        <h1 id="changing_text1" class="mb-0 font-sans text-4xl flex-grow-0 w-min h-min">raine</h1>
+        <h1 id="changing_text2" class="mb-0 font-sans text-4xl flex-grow-0 w-min h-min">does</h1>
+        <h1 id="changing_text3" class="mb-0 font-sans text-4xl flex-grow-0 w-min h-min">code</h1>
+      </main>
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
